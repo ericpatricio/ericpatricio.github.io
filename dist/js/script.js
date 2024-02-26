6207d6aaa17a6c7ac2528eee5bcfcd3d1b70af5c
ericpatricio <ericwpatricio@gmail.com>
2024-02-26 16:38:05 -0500

    update index.html

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,30 +3,63 @@
 // Navbar on scroll background color dark | social media icons display none
 const navbar = document.querySelector(".navbar");
 const socialIcons = document.querySelector(".social-media");
-const navToggler = document.querySelector(".navbar-toggler");
-const collapse = document.querySelector(".navbar-collapse");
+
+const the_animation = document.querySelectorAll(".animation");
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.5,
+};
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("scroll-animation");
+    }
+  });
+}, options);
+
+for (let i = 0; i < the_animation.length; i++) {
+  const elements = the_animation[i];
+
+  observer.observe(elements);
+}
+
+// select the things we need form the DOM
+let hamburger = document.querySelector(".hamburger");
+let navMenu = document.querySelector(".navbar-menu");
+let navMenuUl = document.querySelector(".navbar-menu ul");
+let navLinks = document.querySelectorAll(".navbar-menu li");
+
+// toggle nav on click of hamburger menu icon
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-active");
+  // burger animation
+  hamburger.classList.toggle("toggle");
+});
+// close nav by clicking on list items
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    if (hamburger.classList.contains("toggle")) {
+      hamburger.classList.remove("toggle");
+    }
+    if (navMenu.classList.contains("nav-active")) {
+      navMenu.classList.remove("nav-active");
+    }
+  });
+});
+
 window.onscroll = function () {
   let top = window.scrollY;
-  if (top >= 100) {
-    navbar.classList.add("navbarDark");
+
+  if (top) {
+    navbar.classList.add("navbarOnScroll");
   } else {
-    navbar.classList.remove("navbarDark");
-  }
-  if (top >= 2800) {
-    socialIcons.style.display = "none";
-  } else {
-    socialIcons.style.display = "flex";
+    navbar.classList.remove("navbarOnScroll");
   }
 };
-// /////////////////////////
-// Add eventlistener to document object to look for class navbar show
-document.addEventListener("click", function (e) {
-  if (e.target === navToggler) {
-    collapse.classList.add("show");
-  } else {
-    collapse.classList.remove("show");
-  }
-});
+
 // //////////////////////////////////////
 // Enable tooltips with bootstrap
 const tooltipTriggerList = document.querySelectorAll(
@@ -36,13 +69,7 @@ const tooltipList = [...tooltipTriggerList].map(
   tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl)
 );
 // ////////////////////////////////////////////////////////////
-// Contact form;
-document.querySelector("#contact-form").addEventListener("submit", e => {
-  e.preventDefault();
-  e.target.elements.name.value = "";
-  e.target.elements.email.value = "";
-  e.target.elements.message.value = "";
-});
+
 // Copyright footer
 const year = document.querySelector("#current-year");
 year.innerHTML = new Date().getFullYear();
